faf7caff7f4241236f7ccfd9ad1f9f4193aa1596
JustThePix <justthepixuser@hotmail.com>
2023-10-21 16:17:53 -0500

    Configure GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
+  site: "https://JustThePix.github.io",
+  base: "My-portfolio",
 });
